75362c61102c80ea2aa86803ba666dca34c78fe5
Aslanchen_TheOne <122560007@163.com>
2017-02-22 14:45:21 +0800

    粘包处理

--- SocketTcp/Server/SocketServer.cs
+++ SocketTcp/Server/SocketServer.cs
@@ -262,13 +262,23 @@ namespace SocketTcp.Server
             memStream.Write(bytes, 0, length);
             //Reset to beginning
             memStream.Seek(0, SeekOrigin.Begin);
-            while (RemainingBytes() > 0)
+            while (RemainingBytes() > 4)
             {
-                MemoryStream ms = new MemoryStream();
-                BinaryWriter writer = new BinaryWriter(ms);
-                writer.Write(reader.ReadBytes(length));
-                ms.Seek(0, SeekOrigin.Begin);
-                OnReceivedMessage(client, ms);
+                int messageLen = reader.ReadInt32();
+                if (RemainingBytes() >= messageLen)
+                {
+                    MemoryStream ms = new MemoryStream();
+                    BinaryWriter writer = new BinaryWriter(ms);
+                    writer.Write(reader.ReadBytes(length));
+                    ms.Seek(0, SeekOrigin.Begin);
+                    OnReceivedMessage(client, ms);
+                }
+                else
+                {
+                    //Back up the position two bytes
+                    memStream.Position = memStream.Position - 4;
+                    break;
+                }
             }
             //Create a new stream with any leftover bytes
             byte[] leftover = reader.ReadBytes((int)RemainingBytes());
