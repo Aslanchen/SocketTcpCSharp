948ac0d87a91b80af6356dc12c187d55ebdff9f8
Aslanchen_TheOne <122560007@163.com>
2017-02-24 13:25:59 +0800

    线程安全处理

--- SocketTcp/Common/MsgCenter.cs
+++ SocketTcp/Common/MsgCenter.cs
@@ -1,28 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace SocketTcp.Common
 {
     public class MsgCenter : Singleton<MsgCenter>
     {
-        private static Dictionary<ushort, Action<byte[]>> _processor = new Dictionary<ushort, Action<byte[]>>();
+        private static ConcurrentDictionary<ushort, Action<byte[]>> _processor = new ConcurrentDictionary<ushort, Action<byte[]>>();
 
         public void OnMsg(ushort msg_type, byte[] data)
         {
             if (_processor.ContainsKey(msg_type))
             {
-                _processor[msg_type](data);
+                Action<byte[]> action;
+                _processor.TryGetValue(msg_type, out action);
+                action(data);
             }
         }
 
         public void RegisterMsg(ushort msg_type, Action<byte[]> action)
         {
-            _processor[msg_type] = action;
+            _processor.TryAdd(msg_type, action);
         }
 
         public void UnregisterMsg(ushort msg_type)
         {
-            _processor.Remove(msg_type);
+            Action<byte[]> action;
+            _processor.TryRemove(msg_type, out action);
         }
     }
 }
